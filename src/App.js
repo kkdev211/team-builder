51b5179b644d12acbdfaba6e0c33e12af12f0cc3
kkdev211 <kkdev211@users.noreply.github.com>
2020-02-19 11:00:35 -0500

    stuff on the page..yay!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactDOM from "react-dom";
+import MemberForm from './Components/MemberForm';
 import logo from './logo.svg';
 import './App.css';
-import Form from './Components/Form';
+
 
 
 function App() {
+ //1- set UseState
+ const [teamMembers, setTeamMembers] = useState([
+   {
+    id:1,
+    name:"Bugs Bunny",
+    email: "lovesCarrots@acme.com",
+    role: "Lead Mischief Maker"
+   }
+]);
+//6 create function that will update 'team member' with param values passed
+const addNewTeamMember = member => {
+  const newTeamMember ={
+    id: Date.now(),
+    title: member.title
+  };
+  setTeamMembers([...teamMembers, newTeamMember])
+}
   return (
     <div className="App">
       <header className="App-header">
+      <h1> ACME Team Members</h1>
+      {/*7 pass addNewTeamMember as a prop into Form */}
+      <MemberForm addNewTeamMember={addNewTeamMember} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,6 +45,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/MemberForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const MemberForm = props => {
+console.log(props);
+    //3 set up state for Form
+ const [teamMember, setTeamMember] = useState({
+    id: "initial id",
+    name: "initial name",
+    email: "initial email",
+    role: "initial role"
+
+ });
+ //4 set up OnChange event when input text changes
+ const handleChanges = event => {
+     console.log("event", event.target.value);
+     console.log("name", event.target.name);
+     //5 use text input value to update state
+     setTeamMember({...teamMember, [event.target.name]:event.target.value});
+ };
+ //8 create submit form function with addNewTeamMember prop
+ const submitForm = event => {
+     console.log("submitting!");
+     event.preventDefault();
+     props.addNewTeamMember(teamMember);
+     setTeamMember({id: "", name: "", email: "", role: ""});
+     // last step- clear form
+ };
+
+//1&2 create basic <form> with <input> and add label for htmlFor
+return(
+    <form onSubmit= {submitForm}>
+        <label htmlFor="name">Name</label>
+        <input 
+            type="text" 
+            placeholder="Enter name" 
+            id="name"
+            name="name"
+            value={teamMember.title}
+            onChange={handleChanges}/>
+         <label htmlFor="note">Note</label>
+    <textarea 
+        id="note" 
+        name="body" 
+        placeholder="Add your note" 
+        value={teamMember.body} 
+        onChange={handleChanges}/>
+      {/* Step 9: Submit form with button and onSubmit */}
+      <button type="submit">Add Member</button>
+    </form>
+ );
+};
+export default MemberForm;
