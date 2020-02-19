a3677893d9db494cbb5640c464e4a0f9984842da
kkdev211 <kkdev211@users.noreply.github.com>
2020-02-18 23:24:52 -0500

    set up form.js

---
+++ src/Components/Form.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const MemberForm = props => {
+console.log(props);
+ const [teamMember, setTeamMember] = useState([]);
+};
+<form>
+  <label htmlFor="title">Title</label>
+  <input type="text" placeholder="Enter title" 
+/>
+</form>
